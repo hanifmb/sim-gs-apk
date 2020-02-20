9eae98cf97260277a0b98239a631443d492e1f97
Hanif <hanifdiyer@gmail.com>
2020-02-20 22:12:53 +0700

    Add mission route and update QR Decoder

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -1,8 +1,10 @@
 package jp.jaxa.iss.kibo.rpc.defaultapk;
 
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.util.Log;
 
+import gov.nasa.arc.astrobee.Kinematics;
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
 
 import gov.nasa.arc.astrobee.Result;
@@ -10,6 +12,7 @@ import gov.nasa.arc.astrobee.types.Point;
 import gov.nasa.arc.astrobee.types.Quaternion;
 
 import com.google.zxing.BinaryBitmap;
+import com.google.zxing.NotFoundException;
 import com.google.zxing.RGBLuminanceSource;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.common.HybridBinarizer;
@@ -27,18 +30,30 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
  */
 
 public class YourService extends KiboRpcService {
+
+    Quaternion current_orientation;
+    Point current_position;
+
     @Override
     protected void runPlan1(){
         api.judgeSendStart();
 
-        moveToWrapper(11.5, -5.75, 4.5, 0, 0, 0, 1);
-        //moveToWrapper(11, -6, 5.55, 0, -0.7071068, 0, 0.7071068);
-        //moveToWrapper(11, -5.5, 4.33, 0, 0.7071068, 0, 0.7071068);
-        String hasil = decodeQRCode();
-        Log.i("QR code", hasil);
+        //kinematics_thread thread = new kinematics_thread();
+        //thread.execute();
+
+        moveToWrapper(11.37, -5.75, 4.5, 0, 0, 0, 1, 0, true);
+        moveToWrapper(11, -6, 5.45, 0, -0.7071068, 0, 0.7071068, 1, true);
+        moveToWrapper(11, -5.5, 4.43, 0, 0.7071068, 0, 0.7071068, 2, true);
+
+        moveToWrapper(10.45, -6.45, 4.7, 0, 0, 0.7071068, -0.7071068, 0, false);
+        moveToWrapper(10.45, -6.75, 4.7, 0, 0, 0, 1, 0, false);
+        moveToWrapper(10.95, -6.75, 4.7, 0, 0, 0, 1, 0, false);
+
+        moveToWrapper(11.40, -8, 5, 0, 0, 0, 1, 4, true);
+        moveToWrapper(11, -7.7, 5.45, 0, -0.7071068, 0, 0.7071068, 5, true);
+        moveToWrapper(10.40, -7.5, 4.7, 0, 0, 1, 0, 3, true);
 
         api.laserControl(true);
-
         api.judgeSendFinishSimulation();
     }
 
@@ -55,40 +70,65 @@ public class YourService extends KiboRpcService {
     // You can add your method
     private void moveToWrapper(double pos_x, double pos_y, double pos_z,
                                double qua_x, double qua_y, double qua_z,
-                               double qua_w){
+                               double qua_w, int targetQR, boolean scanQR){
 
         final int LOOP_MAX = 3;
         final Point point = new Point(pos_x, pos_y, pos_z);
         final Quaternion quaternion = new Quaternion((float)qua_x, (float)qua_y,
                                                      (float)qua_z, (float)qua_w);
 
-        Result result = api.moveTo(point, quaternion, true);
+        Result result = api.moveTo(point, quaternion, false);
+        String QRString = "not found";
 
         int loopCounter = 0;
         while(!result.hasSucceeded() || loopCounter < LOOP_MAX){
-            result = api.moveTo(point, quaternion, true);
+            result = api.moveTo(point, quaternion, false);
+
+            if (scanQR == true){
+                QRString = decodeQRCode();
+            }
+
+            if(QRString != "not found"){
+                api.judgeSendDiscoveredQR(targetQR, QRString);
+                Log.d("QR Data",QRString);
+
+                break;
+            }
+            else{
+                Log.d("QR Data:",QRString);
+            }
+
             ++loopCounter;
         }
     }
+
 
     private String decodeQRCode() {
         try {
             QRCodeReader reader = new QRCodeReader();
 
+            //getting navigation cam bitmap data
             Bitmap navcam = api.getBitmapNavCam();
+
             BinaryBitmap dicek = null;
             com.google.zxing.Result result = null;
 
+            //Getting navigation cam parameters
             int width = navcam.getWidth();
             int height = navcam.getHeight();
             int[] pixels = new int[width * height];
+
+
             navcam.getPixels(pixels, 0, width, 0, 0, width, height);
             RGBLuminanceSource source = new RGBLuminanceSource(width, height, pixels);
             dicek = new BinaryBitmap(new HybridBinarizer(source));
+
+            //Decoding QR data
             result = reader.decode(dicek);
+
             return result.getText();
         } catch (Exception e) {
-            return "no QR";
+            return "not found";
         }
     }
 
